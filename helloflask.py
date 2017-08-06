b9d3c45ae3d3192af053dfad7e03989bc4f3c266
Bowei Liu <liubowei19961227@gmail.com>
2017-08-06 15:15:18 +1000

    basic web frame

--- helloflask.py
+++ helloflask.py
@@ -1,9 +1,35 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask import request
+
+
+app = Flask(__name__,static_url_path = '/static')
+
+
+
+
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+  return render_template('try1.html')
+
+
+
+
+@app.route("/play_music",methods=['POST'])
+def play_music():
+	return "play music"
+
+
+@app.route("/download_music",methods=['POST'])
+def dowload_music():
+	return "download music"
+
+
+@app.route("/read_input", methods=['POST'])
+def login():
+    return request.form['username']
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
