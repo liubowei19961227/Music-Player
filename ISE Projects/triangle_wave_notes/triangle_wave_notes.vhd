254536ce264fb083d4513661fdb311e8c644b8d1
RyanKlugman <RyanKlugman@users.noreply.github.com>
2017-08-20 22:33:38 +1000

    sine wave

--- ISE Projects/triangle_wave_notes/triangle_wave_notes.vhd
+++ ISE Projects/triangle_wave_notes/triangle_wave_notes.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_SIGNED.ALL;
-use IEEE.STD_LOGIC_SIGNED.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -19,6 +19,16 @@ end triangle_wave_notes;
 
 architecture Behavioral of triangle_wave_notes is
 
+component division
+	port (
+	clk: in std_logic;
+	rfd: out std_logic;
+	dividend: in std_logic_vector(31 downto 0);
+	divisor: in std_logic_vector(31 downto 0);
+	quotient: out std_logic_vector(31 downto 0);
+	fractional: out std_logic_vector(31 downto 0));
+end component;
+
 type note_array is array (0 to 13) of integer range 101239 to 191110;
 
 constant note_c4 : integer := 191110;
@@ -32,7 +42,7 @@ constant note_b4 : integer := 101239;
 constant music : note_array := (note_c4, note_c4, note_a4, note_a4, note_b4, note_b4, note_a4, note_f4, note_f4, note_e4, note_e4, note_d4, note_d4, note_c4);
 
 constant second_cc : integer := 100000000;
-constant pwm_block_cc : integer := 1000;
+constant pwm_block_cc : integer := 100000;
 
 signal pwm_cc : integer range 0 to pwm_block_cc - 1 := 0;
 signal pwm_block_partial : integer range 0 to pwm_block_cc - 1 := 0;
@@ -43,10 +53,28 @@ signal note_partial : integer range 0 to note_c4 - 1 := 0;
 signal second_partial : integer range 0 to second_cc -1 := 0;
 signal second_count : integer range 0 to 13 := 0;
 
-signal square_wave: integer range -1 to 1 := 1;
+signal square_wave: integer range 0 to 1 := 0;
 signal pulse: std_logic := '0';
 
+signal rfd : std_logic;
+signal dividend : std_logic_vector(31 downto 0);
+signal divisor : std_logic_vector(31 downto 0);
+signal quotient : std_logic_vector(31 downto 0);
+signal fractional : std_logic_vector(31 downto 0);
+
 begin
+
+divider : division
+		port map (
+			clk => clk,
+			rfd => rfd,
+			dividend => dividend,
+			divisor => divisor,
+			quotient => quotient,
+			fractional => fractional);
+			
+dividend <= std_logic_vector(to_unsigned(pwm_block_cc * note_partial, dividend'length));
+divisor <= std_logic_vector(to_unsigned(note_cc, divisor'length));
 
 process (clk)
 begin
@@ -58,15 +86,19 @@ begin
         else
             second_partial <= second_partial + 1;
         end if;
-        if (note_partial = note_cc - 1) then
-            note_partial <= 0;
-            square_wave <= square_wave * (-1);
-        else
+		  if note_partial = 0 then
+				square_wave <= 0;
+        elsif note_partial = note_cc - 1 then
+            square_wave <= 1;
+		  end if;
+        if square_wave = 0 then
             note_partial <= note_partial + 1;
+		  else
+				note_partial <= note_partial - 1;
         end if;
 	     if (pwm_block_partial = pwm_block_cc - 1) then
             pwm_block_partial <= 0;
-				pwm_cc <= square_wave * pwm_block_cc * note_partial / note_cc;
+				pwm_cc <=  to_integer(unsigned(quotient));
 				pulse <= '1';
         else
             pwm_block_partial <= pwm_block_partial + 1;
